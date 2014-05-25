5f0bd08c65afab19cce119228b6157154aea18cd
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2014-05-25 16:58:04 -0400

    Making String field access easier - <Mojo>

--- src/com/mojo/Accessor.java
+++ src/com/mojo/Accessor.java
@@ -1,9 +1,13 @@
 package com.mojo;
 
+import com.sun.tools.hat.internal.model.JavaField;
 import com.sun.tools.hat.internal.model.JavaHeapObject;
 import com.sun.tools.hat.internal.model.JavaObject;
 import com.sun.tools.hat.internal.model.JavaThing;
+import com.sun.tools.hat.internal.model.JavaValueArray;
 import com.sun.tools.hat.internal.oql.OQLEngine;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Accessor {
   private OQLEngine engine;
@@ -20,4 +24,24 @@ public class Accessor {
 
     return actualField;
   }
+
+  public String asString(String fieldName, JavaHeapObject o) throws Exception {
+    return asString(get(fieldName, o));
+  }
+
+  public String asString(JavaHeapObject o) throws Exception {
+    return new String((char[]) ((JavaValueArray) o).getElements());
+  }
+
+  public Map<String, JavaThing> fields(JavaHeapObject o) {
+    JavaObject obj = (JavaObject) o;
+    HashMap<String, JavaThing> allFields = new HashMap<>();
+    JavaField[] fieldsForInstance = obj.getClazz().getFieldsForInstance();
+    for (JavaField f : fieldsForInstance) {
+      JavaThing fieldObj = obj.getField(f.getName());
+      allFields.put(f.getName(), fieldObj);
+    }
+    return allFields;
+  }
+
 }
--- src/com/mojo/Main.java
+++ src/com/mojo/Main.java
@@ -1,32 +1,41 @@
 package com.mojo;
 
+import com.sun.tools.hat.internal.model.JavaField;
 import com.sun.tools.hat.internal.model.JavaHeapObject;
-import com.sun.tools.hat.internal.model.JavaValueArray;
+import com.sun.tools.hat.internal.model.JavaObject;
+import com.sun.tools.hat.internal.model.JavaThing;
 import com.sun.tools.hat.internal.model.Snapshot;
 import com.sun.tools.hat.internal.oql.OQLEngine;
 import com.sun.tools.hat.internal.oql.OQLException;
 import com.sun.tools.hat.internal.oql.ObjectVisitor;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) throws IOException, OQLException {
-    Snapshot snapshot = new NIOHeapLoader().load("C:/Projects/eclipse-mat/heap.bin");
+    final Snapshot snapshot = new NIOHeapLoader().load("C:/Projects/eclipse-mat/heap.bin");
 //    Snapshot snapshot = new HeapLoader().load("C:/Projects/eclipse-mat/heap.bin");
 
     OQLEngine oqlEngine = new OQLEngine(snapshot);
     CountingVisitor counter = new CountingVisitor();
-//    ObjectVisitor inspector = new InspectingVisitor(oqlEngine, snapshot,
-//        new JavaHeapObjectCallback() {
-//          @Override public void run(JavaHeapObject javaObj, Accessor accessor) throws Exception {
-//            if (javaObj.toString().contains("java.lang.Thread")) {
-//              System.out.println("It is a thread!!");
-//              JavaValueArray name = (JavaValueArray) accessor.get("name", javaObj);
-//              System.out.println(String.valueOf((char[]) name.getElements()));
-//            }
-//          }
-//        });
-//    ObjectVisitor compositeVisitor = new CompositeVisitor(counter, inspector);
-    oqlEngine.executeQuery("select t from java.lang.Thread t", counter);
+    ObjectVisitor inspector = new InspectingVisitor(oqlEngine, snapshot,
+        new JavaHeapObjectCallback() {
+          @Override public void run(JavaHeapObject obj, Accessor accessor) throws Exception {
+            if (obj.toString().contains("java.lang.Thread")) {
+              System.out.println("It is a thread!!");
+              System.out.println("NAME=" + accessor.asString("name", obj));
+              Map<String, JavaThing> fields = accessor.fields(obj);
+              for (String k : fields.keySet()) {
+                System.out.println(k + " = " + fields.get(k));
+              }
+            }
+          }
+        }
+    );
+
+    ObjectVisitor compositeVisitor = new CompositeVisitor(counter, inspector);
+    oqlEngine.executeQuery("select t from java.lang.Thread t", compositeVisitor);
     if (counter.getCount() > 6) System.out.println("Too many threads!!!");
     System.out.println(counter.getCount());
   }
